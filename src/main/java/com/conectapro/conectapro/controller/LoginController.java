baa620554d9a90c81ef466b5031c6dd82d0da176
Charles Santos <charles.santos.dev@gmail.com>
2025-02-01 21:24:46 -0300

    feat: Adição de arquivos de log

--- src/main/java/com/conectapro/conectapro/controller/LoginController.java
+++ src/main/java/com/conectapro/conectapro/controller/LoginController.java
@@ -2,7 +2,10 @@ package com.conectapro.conectapro.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +20,8 @@ import com.conectapro.conectapro.service.UserService;
 @RestController
 @RequestMapping("/api")
 public class LoginController {
+
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 	
 	@Autowired
 	private UserService userService;
@@ -41,6 +46,20 @@ public class LoginController {
     	
     	response.setCode(0);
     	response.setMessage("ok");
+    	
+        return response;
+    }
+	
+	@GetMapping("/teste")
+    public ResponseDTO teste() {
+    	
+		
+    	ResponseDTO response = new ResponseDTO();
+    	
+    	logger.debug("Mensagem de Teste...");
+    	
+    	response.setCode(100);
+    	response.setMessage("Teste...");
     	
         return response;
     }
